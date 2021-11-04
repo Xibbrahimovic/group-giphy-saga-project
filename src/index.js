9dd428581c74b60b9d4f963587b73f69bf2b3c6b
Kongxib Vue <kongcvue@gmail.com>
2021-11-04 14:30:42 -0500

    index set up complete

--- src/index.js
+++ src/index.js
@@ -18,7 +18,88 @@ function* rootSaga() {
     yield takeEvery('UPDATE_FAVORITE', updateFavorite);
 }
 
+function* fetchResults(action){
+    try{
+        let searchTerm = action.payload;
+        const response = yield axios.get(`/api/search/${searchTerm}`)
+        //sends results to searchReducer
+        yield put({type: 'SET_RESULTS', payload: response.data})
+    }
+    catch{
+        console.log(err);
+        put({type: 'FETCH_RESULTS_ERROR'})
+    }
+}
+
+function* fetchFavorites(){
+    try{
+        const response = yield axios.get('/api/favorite')
+        //sends results to favoritesReducer
+        yield put({type: 'SET_FAVORITES', payload: response.data})
+    }
+    catch{(err)
+        console.log(err);
+        put({type: 'FETCH_FAVORITES_ERROR'})
+    }
+}
+
+function* addFavorite(action){
+    try{
+        yield axios.post('/api/favorite', action.payload)
+        yield put({type: 'FETCH_FAVORITES'})
+    } catch(error){
+        console.log(err);
+        yield put({type: 'POST_FAVORITES_ERROR'})
+    }
+}
+
+function* updateFavorite(action){
+    try{
+        yield axios.put(`/api/favorite/:${favId}`);
+        yield put({type: 'FETCH_FAVORITES'})
+    }
+    catch(err){
+        console.log(err);
+        yield put({type: 'POST_ERROR'})
+    }
+}
+
+//reducer to store favorites 
+const favoriteReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_FAVORITES':
+            return action.payload;
+        default: 
+            return state;
+}
+}
 
 
+//reducer to append to DOM, only needs GET request
+const searchReducer = (state = [], action) => {
+    switch(action.type) {
+        case 'SET_RESULTS':
+            return action.payload;
+        default: 
+            return state;
+    }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+    combineReducers({
+        searchReducer,
+        favoriteReducer
+    }),
+    applyMiddleware(sagaMiddleware, logger)
+);
+
+createSagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+    </Provider>, 
+    document.getElementById('root'));
