ac97fd184c07f4e021954ddd46e169c63e278b4e
Kongxib Vue <kongcvue@gmail.com>
2021-11-04 13:38:48 -0500

    root saga set up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import logger from 'redux-logger';
+import createSagaMiddleware from '@redux-saga/core';
+import {takeEvery, put} from 'redux-saga/effects';
+import axios from 'axios';
+
 import App from './components/App/App';
+
+function* rootSaga() {
+    yield takeEvery('FETCH_RESULTS', fetchResults);
+    yield takeEvery('FETCH_FAVORITES', fetchFavorites);
+    yield takeEvery('ADD_FAVORITE', addFavorite);
+    yield takeEvery('UPDATE_FAVORITE', updateFavorite);
+}
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
